48e54e702eebea02df1a4a94dd650395f81e1a4f
Rodrigo Goncalves de Oliveira <rodrigo@thecoreme.org>
2014-06-22 19:58:22 -0400

    FAIL together

--- src/flappyblock.asm
+++ src/flappyblock.asm
@@ -1,29 +1,30 @@
 ; '2600 for Newbies
 ; Session 13 - Playfield
-
-
-
-
-
                 processor 6502
                 include "vcs.h"
                 include "macro.h"
 
-
 ;------------------------------------------------------------------------------
 PATTERN         = $80                  ; storage location (1st byte in RAM)
 TIMETOCHANGE    = 20                   ; speed of "animation" - change as desired
+
+;HOR_RES        = 160                 ; Horizontal Resolution
+BLOCK_HEIGHT    = 18
+BLOCK_GFX       = %11111111
 ;------------------------------------------------------------------------------
 
                 SEG
                 ORG $F000
 
-Reset
+; block position
+P0VPos          ds      1
+
+Reset:
    ; Clear RAM and all TIA registers
                 ldx #0
                 lda #0
 
-Clear           sta 0,x
+Clear:          sta 0,x
                 inx
                 bne Clear
 
@@ -38,72 +39,140 @@ Clear           sta 0,x
                 ldy #0                 ; "speed" counter
        ;------------------------------------------------
 
-StartOfFrame
+                JSR Init
+MainLoop:
+                JSR StartOfFrame
+                JSR VerticalBlank
+                JSR Picture
+                ;JSR ShowBlock
+                JSR Overscan
+                JMP MainLoop
+
+Init:           SUBROUTINE
+                    ; Init objects colors
+                    LDA #$A0
+                    STA COLUP0          ; Player 0 color
+                    ;LDA #RED
+                    ; Init objects vertical position
+                    LDA #(192/2)
+                    STA P0VPos          ; Player 0 vertical position
+                    ; Init objects horizontal position
+                    STA HMCLR
+                    LDX #0              ; Player 0
+                    LDA #160/2-8    ; TODO calculate horizontal center
+                    JSR Position
+                    STA WSYNC
+                    STA HMOVE
+                RTS
+
+Position:       SUBROUTINE
+                    SEC
+                    STA     WSYNC
+.divideby15         SBC     #15
+                    BCS     .divideby15
+                    EOR     #7
+                    ASL
+                    ASL
+                    ASL
+                    ASL
+                    STA.WX  HMP0,x
+                    STA     RESP0,x
+                RTS
+
+StartOfFrame:
+                SUBROUTINE
    ; Start of new frame
    ; Start of vertical blank processing
 
-                lda #0
-                sta VBLANK
+                    lda #0
+                    sta VBLANK
 
-                lda #2
-                sta VSYNC
+                    lda #2
+                    sta VSYNC
 
-                sta WSYNC
-                sta WSYNC
-                sta WSYNC               ; 3 scanlines of VSYNC signal
+                    sta WSYNC
+                    sta WSYNC
+                    sta WSYNC               ; 3 scanlines of VSYNC signal
 
-                lda #0
-                sta VSYNC
-       ;------------------------------------------------
-       ; 37 scanlines of vertical blank...
-                ldx #0
-VerticalBlank
-                sta WSYNC
-                inx
+                    lda #0
+                    sta VSYNC
+           ;------------------------------------------------
+           ; 37 scanlines of vertical blank...
+                    ldx #0
+                RTS
+VerticalBlank:
+                SUBROUTINE
+                    sta WSYNC
+                    inx
 
-                cpx #37
-                bne VerticalBlank
-       ;------------------------------------------------
-       ; Handle a change in the pattern once every 20 frames
-       ; and write the pattern to the PF1 register
-                iny                    ; increment speed count by one
-                cpy #TIMETOCHANGE      ; has it reached our "change point"?
+                    cpx #37
+                    bne VerticalBlank
+           ;------------------------------------------------
+           ; Handle a change in the pattern once every 20 frames
+           ; and write the pattern to the PF1 register
+                    iny                    ; increment speed count by one
+                    cpy #TIMETOCHANGE      ; has it reached our "change point"?
 
-                bne notyet             ; no, so branch past
-                ldy #0                 ; reset speed count
-                inc PATTERN            ; switch to next pattern
+                    bne .notyet             ; no, so branch past
+                    ldy #0                 ; reset speed count
+                    inc PATTERN            ; switch to next pattern
 
-notyet
-                lda PATTERN            ; use our saved pattern
-                sta PF1                ; as the playfield shape
+.notyet
+                    lda PATTERN            ; use our saved pattern
+                    sta PF1                ; as the playfield shape
 
-       ;------------------------------------------------
-       ; Do 192 scanlines of color-changing (our picture)
-                ldx #0                 ; this counts our scanline number
-Picture
-                stx COLUBK             ; change background color (rainbow effect)
-                sta WSYNC              ; wait till end of scanline
+           ;------------------------------------------------
+           ; Do 192 scanlines of color-changing (our picture)
+                    ldx #0                 ; this counts our scanline number
+                RTS
+Picture:
+                SUBROUTINE
+                    stx COLUBK             ; change background color (rainbow effect)
+                    sta WSYNC              ; wait till end of scanline
 
-                inx
+                    inx
 
-                cpx #192
-                bne Picture
-       ;------------------------------------------------
+                    cpx #192
+                    bne Picture
+           ;------------------------------------------------
 
-                lda #%01000010
-                sta VBLANK          ; end of screen - enter blanking
+                    lda #%01000010
+                    sta VBLANK          ; end of screen - enter blanking
 
-   ; 30 scanlines of overscan...
+       ; 30 scanlines of overscan...
 
-                ldx #0
-Overscan        sta WSYNC
-                inx
+                    ldx #0
+                RTS
 
-                cpx #30
+ShowBlock:
+                SUBROUTINE
+.screenloop         STA WSYNC
+                    ; Player 0 vertical position
+                    LDX #BLOCK_GFX
+                    TYA
+                    SEC
+                    SBC P0VPos
+                    CMP #BLOCK_HEIGHT
+                    BCC .showBlock
+                    LDX #0
+.showBlock          STX GRP0
+                    LDX #BLOCK_GFX
+                    TYA
+                    SEC
+                    DEY
+                    BNE .screenloop
+                RTS
 
-                bne Overscan
+Overscan:       SUBROUTINE
+                    sta WSYNC
+                    inx
 
-                jmp StartOfFrame
+                    cpx #30
+
+                    bne Overscan
+
+                    jmp StartOfFrame
+                RTS
 
 ;------------------------------------------------------------------------------
 
